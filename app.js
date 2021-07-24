0a09d9759594146c83bfb12516e7bf9284065d73
Baraahaydara <>
2021-07-24 21:09:33 +0000

    java script

---
+++ app.js
@@ -0,0 +1,64 @@
+'use strict'
+let name = prompt('What is your name ?');
+alert(`Welcome ${name}`);
+
+
+let weather = prompt ('Did i like the winter season ?');
+weather= weather.toLowerCase();
+
+if (weather == 'yes' || weather == 'y')
+{
+ alert(`correct answer `);
+}
+else
+{
+  alert(`sorry `);
+}
+// *************************
+let cofee = prompt ('Did i like cofee ?');
+cofee= cofee.toLowerCase();
+
+if (cofee == 'no' || cofee == 'n')
+{
+ alert(`correct answer `);
+}
+else
+{
+  alert(`sorry`);
+} 
+// ****************************
+let camping = prompt ('Did i like camping ?');
+camping= camping.toLowerCase();
+
+if (camping == 'no' || camping == 'n')
+{
+ alert(`correct answer `);
+}
+else
+{
+  alert(`sorry`);
+}
+// ******************************
+let eat = prompt ('Did i eat too much ?');
+eat= eat.toLowerCase();
+if (eat == 'yes' || eat == 'y')
+{
+ alert(`correct answer `);
+}
+else
+{
+  alert(`sorry`);
+}
+// *********************************
+let food = prompt ('Did i have a healthy livestyle ?');
+food= food.toLowerCase();
+if (food == 'no' || food == 'no')
+{
+ alert(`correct answer `);
+}
+else
+{
+  alert(`sorry`);
+}
+// ******************************
+
